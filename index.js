32144f3fa2f945400a29236ab6b226582d4dfc65
Justice Marks <her@shedaddy.com>
2019-05-22 15:54:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function whisper(string) {
   return string.toLowerCase();
   
 }
+
+function logShout(string) {
+  console.log(shout(string));
+  
+}
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+  if (string === ) {}
+}
